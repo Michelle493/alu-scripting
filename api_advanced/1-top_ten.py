f10b0efa8d821d913b0e917ff901071b85c02b3d
Michelle493 <m.odusanwo@alustudent.com>
2026-02-26 22:39:17 +0000

    api-advanced

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
-"""Print the titles of the first 10 hot posts of a subreddit."""
+"""Print titles of first 10 hot posts of a subreddit."""
 
 import requests
 
 
 def top_ten(subreddit):
-    """Query Reddit API and print first 10 hot post titles."""
+    """Query Reddit API and print first 10 hot posts."""
 
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    headers = {"User-Agent": "ALU-Reddit-Task/0.1"}
+    url = "https://api.reddit.com/r/{}/hot".format(subreddit)
+    headers = {
+        "User-Agent": "python:api.advanced:v1.0 (by /u/alu_student)"
+    }
     params = {"limit": 10}
 
     try:
@@ -16,20 +18,14 @@ def top_ten(subreddit):
             url,
             headers=headers,
             params=params,
-            allow_redirects=False,
-            timeout=10
+            allow_redirects=False
         )
 
         if response.status_code != 200:
             print(None)
             return
 
-        data = response.json().get("data", {})
-        posts = data.get("children", [])
-
-        if not posts:
-            print(None)
-            return
+        posts = response.json().get("data", {}).get("children", [])
 
         for post in posts:
             print(post.get("data", {}).get("title"))
