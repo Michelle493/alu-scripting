13e8396a469ba0b27a80dde577a7cb87054a4a95
Michelle493 <m.odusanwo@alustudent.com>
2026-03-09 15:47:08 +0000

    api-advanced

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -14,7 +14,7 @@ def top_ten(subreddit):
 	print("GOT HERE!!!")
         RESPONSE = requests.get(URL, headers=HEADERS, allow_redirects=False)
         HOT_POSTS = RESPONSE.json().get("data").get("children")
-print(RESPONSE, HOT_POSTS)
+	print(RESPONSE, HOT_POSTS)
         [print(post.get('data').get('title')) for post in HOT_POSTS]
     except Exception:
         print(None)
